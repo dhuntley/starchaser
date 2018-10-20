c266ba8c320585a84342222f17cf280638399a71
Huntley, Daniel <Daniel_Huntley@intuit.com>
2018-10-19 23:54:19 -0600

    Added basic card layout

--- src/js/components/PowerCard.js
+++ src/js/components/PowerCard.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class PowerCard extends Component {
     render() {
+        const {power} = this.props;
         return (
             <div class="power-card panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <h3 class="panel-title">{power.name}</h3>
                 </div>
                 <div class="panel-body">
-                    Panel content
+                    {power.description}
                 </div>
             </div>
         );
--- src/js/components/PowerList.js
+++ src/js/components/PowerList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import PowerCard from './PowerCard';
+import powers from '../../data/powers.json';
 
 class PowerList extends Component {
   render() {
     return (
         <div className='power-list'>
-            <PowerCard />
-            <PowerCard />
-            <PowerCard />
-            <PowerCard />
+        {
+            powers.map(power => {
+                return (
+                    <PowerCard power={power} />
+                )
+            })
+        }
         </div>
     );
   }
