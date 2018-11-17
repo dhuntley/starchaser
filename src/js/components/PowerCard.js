0e9ace96078c4e0dd65a00a75feb1e2c31a1e90d
Quinn McCashin <mccashin@ualberta.ca>
2018-11-17 03:00:47 -0700

    Powercard Colors

--- src/js/components/PowerCard.js
+++ src/js/components/PowerCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class PowerCard extends Component {
     constructor() {
         super();
@@ -20,13 +21,16 @@ class PowerCard extends Component {
         return (
             <div className={`power-card card${this.state.selected ? " selected" : ""}`} onClick={this.handleSelected}>
                 <div className="card-header">
-                    <h3 className="panel-title">{power.name} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {power.playerClass} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {power.playerSubClass} </h3>
+                    <h3 className="panel-title" id="cardHeader">
+                    <div id="cardhead1">{power.name}</div>
+                    <div id="cardhead2"> {power.playerClass[0].toUpperCase() + power.playerClass.substring(1)} </div>
+                    <div id="cardhead3">{power.playerSubClass[0].toUpperCase() + power.playerSubClass.substring(1)}</div>
+                    </h3>
                 </div>
-                <div className="card-body">
+                <div className={"card-body " + power.playerClass} id="bottomtext">
                     Tags: {power.actionTags}<br/>
                     Action Type: {power.actionType}<br/>
                     Description: {power.description}<br/>
-                    
                 </div>
             </div>
         );
