0f0a5f16caab37932fccf0d4c0ae6b463b401b2b
Quinn McCashin <mccashin@ualberta.ca>
2018-11-20 12:58:06 -0700

    Changes to color backgrounds

--- src/js/components/PowerCard.js
+++ src/js/components/PowerCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 class PowerCard extends Component {
     constructor() {
         super();
@@ -22,15 +21,16 @@ class PowerCard extends Component {
             <div className={`power-card card${this.state.selected ? " selected" : ""}`} onClick={this.handleSelected}>
                 <div className="card-header">
                     <h3 className="panel-title" id="cardHeader">
-                    <div id="cardhead1">{power.name}</div>
-                    <div id="cardhead2"> {power.playerClass[0].toUpperCase() + power.playerClass.substring(1)} </div>
-                    <div id="cardhead3">{power.playerSubClass[0].toUpperCase() + power.playerSubClass.substring(1)}</div>
+                        <div id="cardhead1">{power.name}</div>
+                        <div id="cardhead2"> {power.playerClass[0].toUpperCase() + power.playerClass.substring(1)} </div>
+                        <div id="cardhead3">{power.playerSubClass[0].toUpperCase() + power.playerSubClass.substring(1)}</div>
                     </h3>
                 </div>
                 <div className={"card-body " + power.playerClass} id="bottomtext">
-                    Tags: {power.actionTags}<br/>
-                    Action Type: {power.actionType}<br/>
-                    Description: {power.description}<br/>
+
+                        Tags: {power.actionTags.join(', ')}<br/>
+                        Action Type: {power.actionType}<br/>
+                        Description: {power.description}<br/>
                 </div>
             </div>
         );
--- src/js/components/PowerList.js
+++ src/js/components/PowerList.js
@@ -49,10 +49,11 @@ class PowerList extends Component {
                         <div className="col">
                             <div className="form-group">
                                 <label htmlFor="classFilterSelect2">Filter by Sub-Class</label>
-                                <select value={classFilter} className="form-control" id="classFilterSelect2" onChange={(event) => this.handleFilterClass(event.target.value)}>
+                                <select value={classFilter} className="form-control" id="subClassFilterSelect" onChange={(event) => this.handleFilterClass(event.target.value)}>
                                     <option value={'all'}>All</option>
-                                    <option value={'brawler'}>Brawler</option>
-                                    <option value={'marksman'}>Marksman</option>
+                                    <option value={'martialFighter'}>Martial Fighter</option>
+                                    <option value={'weaponMaster'}>Weapon Master</option>
+                                    <option value={'swashbuckler'}>Swashbuckler</option>
                                 </select>
                             </div>
                         </div>
@@ -60,9 +61,9 @@ class PowerList extends Component {
                             <div className="form-group">
                                 <label htmlFor="classFilterSelect3">Filter by Action Level</label>
                                 <select value={classFilter} className="form-control" id="classFilterSelect3" onChange={(event) => this.handleFilterClass(event.target.value)}>
-                                    <option value={'all'}>All</option>
-                                    <option value={'brawler'}>Brawler</option>
-                                    <option value={'marksman'}>Marksman</option>
+                                    <option value={'core'}>Core Actions</option>
+                                    <option value={'standard'}>Standard Talents</option>
+                                    <option value={'superior'}>Superior Talents</option>
                                 </select>
                             </div>
                         </div>
